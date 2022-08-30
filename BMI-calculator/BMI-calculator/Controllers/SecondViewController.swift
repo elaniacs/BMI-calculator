bb350db8625226a5465969703f18244dd1de5e43
elaniacs <carensousaesilva@gmail.com>
2022-08-30 10:53:48 -0300

    implementing SecondViewController

---
+++ BMI-calculator/BMI-calculator/Controllers/SecondViewController.swift
@@ -0,0 +1,24 @@
+//
+//  SecondViewController.swift
+//  BMI-calculator
+//
+//  Created by Cáren Sousa on 30/08/22.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+    
+    var bmiValue = "0.0"
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .red
+        
+        let label = UILabel()
+        label.text = bmiValue
+        label.frame = CGRect(x: 0, y: 0, width: 100, height: 50)
+        view.addSubview(label)
+    }
+}
